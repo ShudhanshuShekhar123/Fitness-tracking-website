ef3dd8f2d4ac9e8d99f7e89a1b11f2155efe9b5b
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2023-07-22 21:00:23 +0530

    background image for categories page is added

--- src/Pages/Exercises_categoryPage.jsx
+++ src/Pages/Exercises_categoryPage.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 // import exerciseImage from '../images/gym_image.jpg'
 import styles from "../Styles/exercise_categories.module.css"
 import { Link } from 'react-router-dom'
-const Exercises_categoryPage = () => {
+const Exercises_CategoryPage = () => {
   return (
+    <div className={styles.mainContainer}>
     <div className={styles.exercises_category_container}>
     <Link to="/gym"><div className={styles.card1}>
       <center className={styles.textInsideCard}>GYM</center>
@@ -17,8 +18,8 @@ const Exercises_categoryPage = () => {
     <div className={styles.card4}>
       <center className={styles.textInsideCard}>CARDIO</center>
     </div>
-  </div>
+  </div></div>
   )
 }
 
-export default Exercises_categoryPage
+export default Exercises_CategoryPage
