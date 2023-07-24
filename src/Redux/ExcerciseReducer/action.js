02e45d8c95beea36dd743259267f31daa74ba656
chetan791 <chetanchauhan795@gmail.com>
2023-07-24 13:14:00 +0530

    finished

--- src/Redux/ExcerciseReducer/action.js
+++ src/Redux/ExcerciseReducer/action.js
@@ -5,15 +5,15 @@ import {
   EXCERCISE_SUCCESS,
 } from "./actionType";
 
-export const getExcercise = (bodypart) => async (dispatch) => {
+export const getExcercise = (obj) => async (dispatch) => {
   dispatch({ type: EXCERCISE_REQUEST });
   try {
     const res = await axios.get(
-      `https://fitness-palace.onrender.com/fitness/exercise?category=gym&type=${bodypart}`,
+      `https://fitness-place.onrender.com/fitness/exercise?category=gym&type=${obj.bodypart}`,
       {
-        //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGI5NmY5YjU3M2JlNWIwZWM5NTlkMGYiLCJpYXQiOjE2OTAwMjk5MTV9.1rvqcMFgBYJ7liboip7AHdKGQ4JWCtutvUN6VaaZfFo
         headers: {
           "Content-Type": "application/json",
+          Authorization: obj.token,
         },
       }
     );
